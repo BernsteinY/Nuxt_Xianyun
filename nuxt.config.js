7a0ed116d22e1fad29483b650dc47de59d7be6ea
Bernstein <xxx@xx.com>
2019-08-02 20:57:21 +0800

    国内飞机票页面

--- nuxt.config.js
+++ nuxt.config.js
@@ -52,7 +52,8 @@ export default {
     {
       src: '@/plugins/localStorage',
       ssr: false
-    }
+    },
+    '@/plugins/axios' //调用插件
   ],
   /*
    ** Nuxt.js dev-modules
---
+++ plugins/axios.js
@@ -0,0 +1,21 @@
+import {
+  Message
+} from 'element-ui'
+
+export default function ({
+  $axios,
+  redirect
+}) {
+  $axios.onError(err => {
+    const {
+      statusCode,
+      message
+    } = err.response.data
+
+    if (statusCode === 400) {
+      Message.warning({
+        message
+      })
+    }
+  })
+}
