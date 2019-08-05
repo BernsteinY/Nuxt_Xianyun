093b96cc1b49b52e856027233bd6e403b10ad26f
Bernstein <xxx@xx.com>
2019-08-05 21:28:09 +0800

    历史记录完成 未跳转订单

--- components/air/searchForm.vue
+++ components/air/searchForm.vue
@@ -203,6 +203,20 @@ export default {
         // url的5个参数
         query: this.form
       });
+
+      // 获取出本地（vuex）的数据
+      const arr = [...this.$store.state.air.history];
+
+      // 新的记录添加到第一个
+      arr.unshift(this.form);
+
+      // 如果长度大于5,只保留5位
+      if (arr.length > 5) {
+        arr.length = 5;
+      }
+
+      // 调用vuex 的方法保存数据
+      this.$store.commit("air/setHistory", arr);
     }
   },
   mounted() {}
--- pages/air/flights.vue
+++ pages/air/flights.vue
@@ -4,7 +4,7 @@
       <!-- 顶部过滤列表 -->
       <div class="flights-content">
         <!-- 过滤条件 -->
-        <div></div>
+        <FlightsFilters :data="cacheFlightsData" @setDataList="setDataList" />
 
         <!-- 航班头部布局 -->
         <FlightsListHead />
@@ -31,6 +31,7 @@
       <!-- 侧边栏 -->
       <div class="aside">
         <!-- 侧边栏组件 -->
+        <FlightsAside />
       </div>
     </el-row>
   </section>
@@ -39,12 +40,24 @@
 <script>
 import FlightsListHead from "@/components/air/flightsListHead.vue";
 import FlightsItem from "@/components/air/flightsItem.vue";
+import FlightsFilters from "@/components/air/flightsFilters.vue";
+import FlightsAside from "@/components/air/flightsAside.vue";
 
 export default {
   data() {
     return {
+      // 缓存一份数据
+      cacheFlightsData: {
+        flights: [],
+        info: {},
+        options: {}
+      },
       // 航班总数据
-      flightsData: {},
+      flightsData: {
+        flights: [],
+        info: {},
+        options: {}
+      },
       //   航班列表数据，用于循环 flightsItem组件, 单独出来是因为要分页
       dataList: [],
       pageIndex: 1, // 显示页数
@@ -54,7 +67,15 @@ export default {
   },
   components: {
     FlightsListHead,
-    FlightsItem
+    FlightsItem,
+    FlightsFilters,
+    FlightsAside
+  },
+  watch: {
+    $route() {
+      this.pageIndex = 1;
+      this.getData();
+    }
   },
   methods: {
     //   获取航班总数据
@@ -65,6 +86,11 @@ export default {
       }).then(res => {
         console.log(res);
         this.flightsData = res.data;
+        // 缓存一份列表数据
+        this.cacheFlightsData = { ...res.data };
+
+        // 总条数
+        this.total = this.flightsData.flights.length;
 
         // 初始化datalist数据
         this.setDataList();
@@ -72,11 +98,16 @@ export default {
     },
 
     // 设置dataList数据
-    setDataList() {
-      const start = (this.pageIndex - 1) * this.pagesize;
-      const end = start + this.pagesize;
-      this.total = this.flightsData.flights.length;
-      this.dataList = this.flightsData.flights.slice(start, end);
+    setDataList(arr) {
+      if (arr) {
+        this.flightsData.flights = arr;
+        this.total = arr.length;
+      }
+      // 修改 dataList的数据
+      this.dataList = this.flightsData.flights.slice(
+        (this.pageIndex - 1) * this.pagesize,
+        (this.pageIndex - 1) * this.pagesize + this.pagesize
+      );
     },
     handleSizeChange(val) {
       this.pagesize = val;
